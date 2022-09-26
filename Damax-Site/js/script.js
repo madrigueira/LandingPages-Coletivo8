28b3547ac49d83ac3bef50cd502ab91bf94596b9
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-09-26 14:15:28 -0300

    Delete Damax-Site directory

--- Damax-Site/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#header-1").load("../html/header-1.html");
-    $("#header-2").load("../html/header-2.html");
-});
-
-function toggleMenu(){
-    document.getElementById("menu").classList.toggle("active")
-}
