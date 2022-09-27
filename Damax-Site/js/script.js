9a8c7980e6b01ab4bde7bf6170330f237bb24a1e
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-09-27 14:17:02 -0300

    bloco 3 pronto tbm

--- Damax-Site/js/script.js
+++ Damax-Site/js/script.js
@@ -17,7 +17,10 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 40,
     loop: true,
-    autoplay: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
