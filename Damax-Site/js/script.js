50b635e34d721c94ed50b44f27ba61641d53d4c5
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-09-27 13:52:16 -0300

    Bloco 2 pronto e responsivo

--- Damax-Site/js/script.js
+++ Damax-Site/js/script.js
@@ -1,8 +1,33 @@
+
+//  Chama os blocos da pasta html pras páginas principais
 $(function(){
     $("#header-1").load("html/header-1.html");
     $("#header-2").load("html/header-2.html");
 });
 
+
+//  Menu Mobile
 function toggleMenu(){
     document.getElementById("menu").classList.toggle("active")
 }
+
+//  Carousel da Home - Bloco 2
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    autoplay: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        850: {
+            slidesPerView: 3,
+        },
+        600: {
+            slidesPerView: 2,
+        }
+    }
+});
